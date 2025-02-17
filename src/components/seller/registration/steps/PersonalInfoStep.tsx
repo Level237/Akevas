74aab922c41fa1338e8bcc86f462df49766b222c
level237 <Bramslevel129@gmail.com>
2025-02-17 12:04:02 +0100

    update

--- src/components/seller/registration/steps/PersonalInfoStep.tsx
+++ src/components/seller/registration/steps/PersonalInfoStep.tsx
@@ -90,6 +90,8 @@ const PersonalInfoStep: React.FC<PersonalInfoStepProps> = ({ data, onUpdate }) =
             type="date"
             id="birthDate"
             name="birthDate"
+            min={new Date().toISOString().split('T')[0]}
+            max={new Date(new Date().setFullYear(new Date().getFullYear() - 18)).toISOString().split('T')[0]}
             value={data.birthDate}
             onChange={handleChange}
             className="py-6"
