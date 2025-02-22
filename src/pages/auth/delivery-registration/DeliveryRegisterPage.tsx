f603ed3ec2dbb7300f540e0ee162a42b4f9357ec
level237 <Bramslevel129@gmail.com>
2025-02-22 12:25:51 +0100

    update

--- src/pages/auth/delivery-registration/DeliveryRegisterPage.tsx
+++ src/pages/auth/delivery-registration/DeliveryRegisterPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   User,
@@ -8,9 +8,16 @@ import {
   CheckCircle,
   ChevronRight
 } from 'lucide-react';
-import AsyncLink from '@/components/ui/AsyncLink';
 import { ScrollRestoration } from 'react-router-dom';
-
+import { PageTransition } from '@/components/ui/page-transition';
+import TopLoader from '@/components/ui/top-loader';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select';
+import { useGetTownsQuery, useGetQuartersQuery } from '@/services/guardService';
 const steps = [
   {
     id: 1,
@@ -44,40 +51,53 @@ const steps = [
   }
 ];
 
+
+const formSchema=z.object({
+  firstName:z.string().min(2,"le prenom doit contenir au moins 2 caractères"),
+  lastName:z.string().min(2,"le nom doit contenir au moins 2 caractères"),
+  email:z.string().email("email invalide"),
+  phone:z.string().min(10,"le numero de telephone doit contenir au moins 10 caractères"),
+  birthDate:z.string().min(10,"la date de naissance est obligatoire"),
+  nationality:z.string().min(2,"la nationalité est obligatoire"),
+  idNumber:z.string().min(2,"le numero de piece d'identité est obligatoire"),
+  address:z.string().min(2,"l'adresse est obligatoire"),
+  city:z.string().min(1,"la ville est obligatoire"),
+  quarter:z.string().min(1,"le quartier est obligatoire"),
+  
+})
 const DeliveryRegisterPage: React.FC = () => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    birthDate: '',
+  const form=useForm<z.infer<typeof formSchema>>({
+    resolver:zodResolver(formSchema),
+    defaultValues:{
+      firstName:'',
+      lastName:'',
+      email:'',
+      phone:'',
+      birthDate:'',
     nationality: '',
     idNumber: '',
-    address: '',
     city: '',
-    country: ''
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData(prev => ({
-      ...prev,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Implement form submission
-    console.log(formData);
-  };
+    quarter: ''
+  }
+}
+);
+const { data: towns, isLoading: townsLoading } = useGetTownsQuery('guard');
+ const { data: quarters, isLoading: quartersLoading } = useGetQuartersQuery('guard');
+  const [townId,setTownId]=useState<string>('')
+  const filteredQuarters = quarters?.quarters.filter((quarter: { town_id: string }) => quarter.town_id === parseInt(townId));
+  const onSubmit=async(values:z.infer<typeof formSchema>)=>{
+    console.log(values)
+  }
 
   return (
+    <PageTransition>
     <div className="min-h-screen bg-gray-50">
+      <TopLoader progress={16.7} />
       <ScrollRestoration/>
       <main className="max-w-7xl mx-auto px-4 py-8">
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold text-gray-900">
+            
             Devenir livreur partenaire
           </h1>
           <p className="mt-2 text-gray-600">
@@ -119,172 +139,254 @@ const DeliveryRegisterPage: React.FC = () => {
         </nav>
 
         {/* Form */}
-        <div className="max-w-2xl mx-auto">
+        <div className="max-w-4xl mx-auto">
           <div className="bg-white rounded-2xl shadow-sm">
             <div className="p-6">
               <h2 className="text-xl font-semibold mb-6">
                 Informations personnelles
               </h2>
 
-              <form onSubmit={handleSubmit} className="space-y-6">
+            <Form {...form}>
+
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
                 <div className="grid max-sm:grid-cols-1 grid-cols-2 gap-6">
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Prénom
-                    </label>
-                    <input
-                      type="text"
+                  
+                    <FormField
+                      control={form.control}
                       name="firstName"
-                      value={formData.firstName}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
-                    />
-                  </div>
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Prénom</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
+                              
+                      />
+                      </FormControl>
+                      <FormMessage />
+                      </FormItem>
+                      )}
+                      />
+                  
 
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Nom
-                    </label>
-                    <input
-                      type="text"
+                  
+                    <FormField
+                      control={form.control}
                       name="lastName"
-                      value={formData.lastName}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Nom</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
-                  </div>
+                  
                 </div>
 
+
                 <div className="grid max-sm:grid-cols-1 grid-cols-2 gap-6">
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Email
-                    </label>
-                    <input
-                      type="email"
+                  
+                    <FormField
+                      control={form.control}
                       name="email"
-                      value={formData.email}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
-                  </div>
+                  
 
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Téléphone
-                    </label>
-                    <input
-                      type="tel"
+                 
+                    <FormField
+                      control={form.control}
                       name="phone"
-                      value={formData.phone}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
-                    />
-                  </div>
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Téléphone</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
+                            />
+                      </FormControl>
+                      <FormMessage />
+                      </FormItem>
+                      )}
+                      />
+                     
                 </div>
 
+
                 <div className="grid max-sm:grid-cols-1 grid-cols-2 gap-6">
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Date de naissance
-                    </label>
-                    <input
-                      type="date"
+                  
+                    <FormField
+                      control={form.control}
                       name="birthDate"
-                      value={formData.birthDate}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Date de naissance</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
+                            />
+                          </FormControl>  
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Nationalité
-                    </label>
-                    <input
-                      type="text"
+                                      <FormField
+                      control={form.control}
                       name="nationality"
-                      value={formData.nationality}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Nationalité</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
                   </div>
-                </div>
 
-                <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-1">
-                    Numéro de pièce d'identité
-                  </label>
-                  <input
-                    type="text"
+
+                  
+               
+
+              
+
+                  
+                  <FormField
+                    control={form.control}
                     name="idNumber"
-                    value={formData.idNumber}
-                    onChange={handleChange}
-                    className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                    required
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Numéro de pièce d'identité</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </div>
-
-                <div>
-                  <label className="block text-sm font-medium text-gray-700 mb-1">
-                    Adresse
-                  </label>
-                  <input
-                    type="text"
-                    name="address"
-                    value={formData.address}
-                    onChange={handleChange}
-                    className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                    required
-                  />
-                </div>
+           
 
                 <div className="grid max-sm:grid-cols-1 grid-cols-2 gap-6">
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Ville
-                    </label>
-                    <input
-                      type="text"
+              
+                    <FormField
+                      control={form.control}
                       name="city"
-                      value={formData.city}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
+                      render={({ field }) => (
+              <FormItem>
+              <FormLabel>Ville</FormLabel>
+              <Select onValueChange={(value) => {
+          field.onChange(value); // Met à jour la valeur du champ
+          setTownId(value); // Met à jour l'état townId
+        }} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selectionnez une ville" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {townsLoading ? (
+                    <SelectItem value="loading">Chargement des villes...</SelectItem>
+                  ) : (
+                    towns.towns.map((town:{id:string,town_name:string}) => (
+                      <SelectItem key={town.id} value={String(town.id)}>
+                        {town.town_name}
+                      </SelectItem>
+                    ))
+                  )}
+                    
+                </SelectContent>
+              </Select>
+              
+              <FormMessage />
+            </FormItem>
+                       
+                      )}
+                    />
+                            <FormField
+                        control={form.control}
+                      name="quarter"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Quartier</FormLabel>
+                          <Select 
+                              onValueChange={field.onChange}  
+                              defaultValue={field.value}
+                            >
+                          <FormControl>
+                            
+                              <SelectTrigger>
+                                <SelectValue placeholder="Selectionnez un quartier" />
+                              </SelectTrigger>
+                               </FormControl>
+                              <SelectContent>
+                               {quartersLoading ? (
+                  <SelectItem value="loading">Chargement des quartiers...</SelectItem>
+                ) : (
+                  filteredQuarters?.map((quarter:{id:string,quarter_name:string}) => (
+                    <SelectItem key={quarter.id} value={String(quarter.id)}>
+                      {quarter.quarter_name}
+                    </SelectItem>
+                  ))
+                )}
+                {filteredQuarters?.length === 0 && (
+                  <SelectItem value="no-quarters">Aucun quartier trouvé,veuillez verifier votre ville</SelectItem>
+                )}
+                              </SelectContent>
+                              </Select>
+                              <FormMessage />
+                              
+                        </FormItem>
+                      )}
                     />
                   </div>
 
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Pays
-                    </label>
-                    <input
-                      type="text"
-                      name="country"
-                      value={formData.country}
-                      onChange={handleChange}
-                      className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-[#ed7e0f] focus:border-transparent"
-                      required
-                    />
-                  </div>
-                </div>
+                    
+           
+
+
+                
+
+                  
+               
 
                 <div className="flex justify-end">
-                  <AsyncLink
-                    to="/delivery/vehicle"
+                  <button
+                    type="submit"
                     className="px-6 py-2 bg-[#ed7e0f] text-white rounded-lg hover:bg-[#ed7e0f]/80 transition-colors flex items-center gap-2"
                   >
                     Suivant
                     <ChevronRight className="w-4 h-4" />
-                  </AsyncLink>
+                  </button>
                 </div>
               </form>
+            </Form>
             </div>
           </div>
 
@@ -323,6 +425,7 @@ const DeliveryRegisterPage: React.FC = () => {
         </div>
       </main>
     </div>
+    </PageTransition>
   );
 };
 
