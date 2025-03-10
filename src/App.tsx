350245c9d98e3cfddb5d592dd78a4e4fc079e3c3
level237 <Bramslevel129@gmail.com>
2025-03-10 20:54:43 +0100

    Remove Sentry error tracking and performance monitoring
    - Remove Sentry-related dependencies from package.json and package-lock.json
    - Delete Sentry initialization code from App component
    - Clean up unused Sentry imports and configurations

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,11 @@ import { routes } from './routes/routes'
 import { LoaderProvider } from './context/LoaderContext'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import * as Sentry from "@sentry/react";
+
 
 function App() {
 
 
-  Sentry.init({
-    dsn: "https://fa9b468024da5f968eabe4017b7ac1d4@o4508954538082304.ingest.us.sentry.io/4508954540179456",
-    integrations: [
-      Sentry.browserTracingIntegration()
-    ],
-    // Tracing
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/]
-  });
   return (
     <LoaderProvider>
       <Provider store={store}>
