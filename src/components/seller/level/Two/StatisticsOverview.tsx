80c1bdf51aa93599b7aac7f937d2fef15abdf66b
level237 <Bramslevel129@gmail.com>
2025-04-17 16:16:12 +0200

    Refactor StatisticsOverview and DashboardPage for improved UI and responsiveness
    - Updated the StatisticsOverview component to hide the "Nouveau produit" label on smaller screens for better mobile responsiveness.
    - Enhanced the DashboardPage layout and styling, including adjustments to padding, spacing, and font sizes for improved readability and user experience.
    - Made minor adjustments to button sizes and icon dimensions for consistency across different screen sizes.

--- src/components/seller/level/Two/StatisticsOverview.tsx
+++ src/components/seller/level/Two/StatisticsOverview.tsx
@@ -44,7 +44,7 @@ export default function StatisticsOverview(sellerData: { sellerData: Seller | nu
             className="flex items-center gap-2 px-4 py-2 bg-transparent text-[#d97100] border border-[#d97100] text-white rounded-xl transition-all duration-300 shadow-md hover:shadow-lg"
           >
             <Plus className="w-5 h-5" />
-            <span>Nouveau produit</span>
+            <span className="max-sm:hidden">Nouveau produit</span>
           </AsyncLink>
           </div>
           
