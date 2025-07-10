8605435f71d0b8c94cb26498853c0d447f93e904
level237 <bramslevel129@gmail.com>
2025-07-10 20:43:36 +0100

    Update service worker and product detail page with new WhatsApp contact number and cache revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t219u7vebs8"
+    "revision": "0.a04of5m9j4o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
