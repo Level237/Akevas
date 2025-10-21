ac47f1f212aac221dfff757648c63ee32565db13
level237 <bramslevel129@gmail.com>
2025-10-21 10:10:06 +0100

    Remove unused PageLoader import from App component, simplifying the code and improving maintainability.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/routes'
 import { LoaderProvider } from './context/LoaderContext'
-import {  useDispatch, useSelector } from 'react-redux'
+import {  useDispatch } from 'react-redux'
 
 import ErrorBoundary, { NetworkBoundary } from './components/errors/error-boundary'
 import { Toaster } from 'sonner'
@@ -11,7 +11,7 @@ import { NuqsAdapter } from 'nuqs/adapters/react'
 import { useCheckAuthQuery } from './services/auth'
 import { useEffect } from 'react'
 import { setAuthInitialized } from './store/authSlice'
-import PageLoader from './components/ui/PageLoader'
+
 
 function App() {
 
