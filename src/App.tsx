4c7b1a712128eccbed04c3e486c064cd5dce0517
level237 <bramslevel129@gmail.com>
2025-11-09 22:43:41 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,9 @@ import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/routes'
 import { LoaderProvider } from './context/LoaderContext'
-import {  useDispatch } from 'react-redux'
-import { Provider } from 'react-redux'
-import { store } from './store'
 import ErrorBoundary, { NetworkBoundary } from './components/errors/error-boundary'
 import { Toaster } from 'sonner'
-import { NuqsAdapter } from 'nuqs/adapters/react'
-import { useCheckAuthQuery } from './services/auth'
-import { useEffect } from 'react'
-import { setAuthInitialized } from './store/authSlice'
+
 
 function App() {
  
