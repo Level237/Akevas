2a2a1f16badef409b180a51aaaa9984f35999434
level237 <bramslevel129@gmail.com>
2025-11-10 12:05:06 +0100

    update

--- src/components/ui/menuNavigation.tsx
+++ src/components/ui/menuNavigation.tsx
@@ -4,12 +4,7 @@ const MenuNavigation = () => {
     return (
         <nav className="flex justify-center w-full">
                         <ul className="flex justify-center items-center gap-8">
-                          <li className="py-4">
-                            <AsyncLink to="/" className="flex text-sm items-center gap-2 text-gray-700 hover:text-orange-500 transition-colors">
-                              
-                              <span  className="text-md">Accueil</span>
-                            </AsyncLink>
-                          </li>
+                          
                           
                           <li className="py-4">
                             <AsyncLink to="/seller/guide" className="flex text-sm items-center gap-2 text-gray-700 hover:text-orange-500 transition-colors">
@@ -17,7 +12,12 @@ const MenuNavigation = () => {
                               <span className="text-md">Guide du vendeur</span>
                             </AsyncLink>
                           </li>
-                          
+                           <li className="py-4">
+                            <AsyncLink to="/seller-registration/personal-info" className="flex text-sm items-center gap-2 text-gray-700 hover:text-orange-500 transition-colors">
+              
+                              <span className="text-md">Devenir vendeur</span>
+                            </AsyncLink>
+                          </li>
                           <li className="py-4">
                             <AsyncLink to="/seller/dashboard" className="flex text-sm items-center gap-2 text-gray-700 hover:text-orange-500 transition-colors">
                              
@@ -31,13 +31,14 @@ const MenuNavigation = () => {
                               <span className="text-md">Marketplace</span>
                             </a>
                           </li>
-                          
                           <li className="py-4">
-                            <AsyncLink to="/contact" className="flex text-sm items-center gap-2 text-gray-700 hover:text-orange-500 transition-colors">
-                              
+                            <a href='https://akevas.com/contact' target="_blank" className="flex text-sm items-center gap-2 text-gray-700 hover:text-orange-500 transition-colors">
+              
                               <span className="text-md">Contact</span>
-                            </AsyncLink>
+                            </a>
                           </li>
+                         
+                          
                         </ul>
                       </nav>    
     )
