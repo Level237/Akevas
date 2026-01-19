e570f6f462a60f450f07eae4e13718e39fab4313
level237 <bramslevel129@gmail.com>
2026-01-19 11:33:20 +0100

    fix: Update API base URL from `api-akevas.akevas.com` to `api.akevas.com` in both authenticated and non-authenticated base queries.

--- src/services/baseQuery.ts
+++ src/services/baseQuery.ts
@@ -2,7 +2,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import Cookies from "universal-cookie";
 
 export const baseQuery = fetchBaseQuery({
-    baseUrl: "https://api-akevas.akevas.com",
+    baseUrl: "https://api.akevas.com",
     //baseUrl: "http://127.0.0.1:8000",
     credentials: 'include',
     prepareHeaders: (headers) => {
